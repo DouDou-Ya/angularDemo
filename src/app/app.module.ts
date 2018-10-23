974d626d1ea87026c0c1b9ff817f280591b47f61
zlt <2051613600@qq.com>
2018-10-23 17:53:48 +0800

    updata

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DirectoryComponent } from './directory/directory.component';
+import { routing } from './app.routes'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DirectoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from "@angular/router"
+import { DirectoryComponent } from "./directory/directory.component"
+import { HomeComponent } from "./home/home.component"
+import { ModuleWithProviders } from "@angular/core";
+
+const appRoutes: Routes = [
+  { path: 'directory', component: DirectoryComponent },
+  { path: '', component: HomeComponent }
+]
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
